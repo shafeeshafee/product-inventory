c1f237ae2ad9103b5da6860bc730cc377896b193
Shafee <shafeelinks@gmail.com>
2020-11-08 18:51:08 -0500

    added bootstrap functionality to site

---
+++ app.js
@@ -0,0 +1,67 @@
+class Product {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    productName() {
+        const { name } = this;
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    cost() {
+        const { price } = this;
+        return `$${Number(price).toFixed(2)}`;
+    }
+
+    amount() {
+        const { quantity } = this;
+        return quantity;
+    }
+    addItem() {
+
+        const { name, price, quantity } = this;
+
+        return `
+        <div class="container">
+            <div class="row">
+                <div class="list-desc col-sm"><span class="p-3 mb-2 bg-dark text-white">Item</span>${this.productName()}</div>
+                <div class="list-desc col-sm"><span class="p-3 mb-2 bg-danger text-white">Price</span>${this.cost()}</div>
+                <div class="list-desc col-sm"><span class="p-3 mb-2 bg-primary text-white">Quantity</span>${this.amount()}</div>
+            </div>
+        </div>`;
+    }
+}
+
+// list div: list-parent
+const list = document.querySelector('.list-parent');
+// button: add-item
+const enterItemButton = document.querySelector('#add-item');
+// product: product-name
+const product = document.querySelector('#product-name');
+// quantity: quantity-of-items
+const quantity = document.querySelector('#quantity-of-items');
+// price: item-price
+const price = document.querySelector('#item-price');
+
+// total: display-total
+const total = document.querySelector('#total');
+
+// enter item on click
+enterItemButton.addEventListener('click', () => {
+    if (product.value && quantity.value && price.value) {
+        let productValue = product.value; let quantityValue = quantity.value; let priceValue = price.value;
+        // new product
+        let newProduct = new Product(productValue, quantityValue, priceValue);
+        // create a li node
+        let newLi = document.createElement('li');
+        newLi.classList.add('list-group-item');
+        newLi.innerHTML = newProduct.addItem();
+        //append it
+        list.append(newLi);
+    } else {
+        alert('Enter all product input fields');
+    }
+    product.value = '', quantity.value = '', price.value = '';
+});
